4769a9210b3621fa11b5996aaaf4dfeac26ed834
meixuanc <123055658@qq.com>
2019-08-02 15:34:12 -0400

    modified import syntax

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Select from '@material-ui/core/select';
+import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
